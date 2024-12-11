cae49b9d3924288589c2b1ef9a71f79ba2df6fd8
tam1444AH <mohammedtalha290@gmail.com>
2024-12-10 23:18:45 -0600

    Enhanced responsivness of Homepage components.

--- client/src/components/FlightsBox.jsx
+++ client/src/components/FlightsBox.jsx
@@ -4,7 +4,7 @@ import { IoMdCloud } from "react-icons/io";
 
 const FlightsBox = () => {
   return (
-    <Card.Body className='container-sm d-flex bg-dark m-5 text-center rounded align-items-center justify-content-center'>
+    <Card.Body className='container-lg d-flex bg-dark text-center rounded align-items-center justify-content-center py-4 mb-4 mx-auto'>
         <div className='text-center'>
             <IoMdCloud className='display-3'/>
             <p className='fs-2'>Search for flights.</p>
--- client/src/components/IATAForm.jsx
+++ client/src/components/IATAForm.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 const IATAForm = () => {
   return (
-    <Row className="w-100 d-flex justify-content-center">
-      <Col xs={11} sm={8} md={6} lg={4} className="p-4 bg-dark rounded">
+    <Container>
+      <Col xs={12} sm={10} md={8} lg={6} className="p-4 bg-dark rounded mb-4 mx-auto">
         <Form>
           <Form.Group>
             <h3 className="text-center mb-4 text-white">Enter the following:</h3>
@@ -17,14 +16,14 @@ const IATAForm = () => {
             <Form.Control
               type="text"
               placeholder="IATA code of airport of departure"
-              className="p-2"
+              className="p-2 text-truncate"
             />
           </Form.Group>
           <Form.Group className="mb-4" controlId="formGroupArrival">
             <Form.Control
               type="text"
               placeholder="IATA code of airport of arrival"
-              className="p-2"
+              className="p-2 text-truncate"
             />
           </Form.Group>
           <div className="d-flex justify-content-center">
@@ -34,7 +33,7 @@ const IATAForm = () => {
           </div>
         </Form>
       </Col>
-    </Row>
+    </Container>
   );
 };
 
